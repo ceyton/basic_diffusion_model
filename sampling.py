43b7ea417a24a3ecaabf6cf8655022902078a389
Shadi <ossaili.apps@gmail.com>
2023-06-28 01:02:12 +0300

    na

--- sampling.py
+++ sampling.py
@@ -75,7 +75,7 @@ def sample_plot_image(device="cpu", IMG_SIZE=28, T=300, model=None):
         img = torch.clamp(img, -1.0, 1.0)
         print(img.shape)
         if i % stepsize == 0:
-            image_list.append(img)
+            image_list.append(img.reshape(3, img_size, img_size))
 
     # Create a grid of images
     image_grid = make_grid(image_list, nrow=5)  # Adjust nrow as needed
